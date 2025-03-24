015902480038827be52ad838e9907429d2d8b7ce
KL-Mdanjelwa <mdanjelwaluthando@gmail.com>
2025-03-24 16:09:27 +0200

    Set up project structure and main files.

--- Part 1.2/Program.cs
+++ Part 1.2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Media;
+using System.Threading;
+using System.Xml.Serialization;
+using NAudio.Wave;
+
+class CyberSecurityBot
+{
+
+    static void Main()
+    {
+    }
+
+    static void DisplayAsciiArt() { }
+
+    static void PlayVoiceGreeting() { }
+
+    static void ChatbotLoop(string userName) { }
+
+    static string GetResponse(string input, string userName) { }
+
